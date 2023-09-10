813ff319fc780f46d0b75032230fd543dbe6de90
Aytuğ <umut-aytug@hotmail.com>
2023-09-11 02:46:41 +0300

    NEW

---
+++ script.js
@@ -0,0 +1,75 @@
+window.PorfolioConfig = {
+    name: 'Umut Aytuğ Semerci',
+    jobTitle: '3rd Grade Computer Engineering Student',
+    jobDescription: 'Passionate about coding and cybersecurity.',
+    skills: [
+        { type: 'HTML', level: 100 },
+        { type: 'CSS', level: 75 },
+        { type: 'JavaScript', level: 75 },
+        { type: 'Node.js', level: 75 },
+        { type: 'Linux', level: 50 },
+        { type: 'C', level: 50 },
+        { type: 'C++', level: 50 },
+        { type: 'Python', level: 25 },
+        { type: 'React', level: 25 },
+        { type: 'MongoDB', level: 25 },
+        { type: 'Git', level: 25 }
+    ],
+    works: [
+        {
+            name: 'DCBell - Discord Music Bot',
+            link: 'https://github.com/uaytug/DcBell-beta-public',
+            description: ' A Discord.js music bot named dcbell, created to play music in Discord voice channels.'
+        },
+        {
+            name: 'Multi TV Page',
+            link: 'https://github.com/uaytug/multitv',
+            description: ' A multi TV page project that allows users to watch multiple TV channels simultaneously.'
+        },
+        {
+            name: 'Voting System in C',
+            link: 'https://github.com/uaytug/Voting-System',
+            description: ' A C program for implementing a voting system with various functionalities.'
+        },
+        {
+            name: 'Wordle Clone in JavaScript',
+            link: 'https://github.com/uaytug/I2DLE',
+            description: ' A JavaScript-based clone of the popular game Wordle, where players guess words.'
+        }
+    ],
+    hobbies: [
+        {
+            name: 'Playing Video Games',
+            description: 'I enjoy playing a variety of video games in my free time.'
+        },
+        {
+            name: 'Coding',
+            description: 'Coding is not just my profession but also my passion.'
+        },
+        {
+            name: 'Music',
+            description: 'I love listening to music and exploring different genres.'
+        }
+    ],
+    contacts: {
+        email: 'umut-aytug@hotmail.com',
+        links: [
+            {
+                name: 'GitHub',
+                link: 'https://github.com/uaytug'
+            },
+            {
+                name: 'LinkedIn',
+                link: 'https://www.linkedin.com/in/uaytug'
+            },
+            {
+                name: 'Instagram',
+                link: 'https://www.instagram.com/u.aytug/'
+            },
+            {
+                name: 'Resume',
+                link: 'assets\\Resume.pdf'
+            }
+        ]
+    }
+};
